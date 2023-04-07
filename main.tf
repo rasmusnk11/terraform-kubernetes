ddfe2d0e32da9e84541dbed9569299ff82f0b535
hatedone01 <rasmusnk@gmail.com>
2023-04-07 15:29:55 +0200

    tekanaid.com

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.2.0"
+    }
+  }
+}
+
 provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
@@ -5,57 +14,75 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+locals {
+  templatevars = {
+    name         = var.name,
+    ipv4_address = var.ipv4_address,
+    ipv4_gateway = var.ipv4_gateway,
+    dns_server_1 = var.dns_server_list[0],
+    dns_server_2 = var.dns_server_list[1],
+    public_key = var.public_key,
+    ssh_username = var.ssh_username
+  }
+}
+
 data "vsphere_datacenter" "datacenter" {
-  name = var.vsphere_datacenter
+  name = var.vsphere-datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "System"
+  name          = var.vm-datastore
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
-  name          = "LAB-SILK"
+  name          = var.vsphere-cluster
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_network" "network" {
-  name          = "Kubernetes (Vlan 110)"
+  name          = var.vm-network
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "ubuntu-temp-22.04.2"
+  name          = "/${var.vsphere-datacenter}/vm/${var.vsphere-template-folder}/${var.vm-template-name}"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_name
+  name             = var.name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus         = 2
-  memory           = 2048
-  guest_id         = "ubuntu64Guest"
+  num_cpus             = var.cpu
+  num_cores_per_socket = var.cores-per-socket
+  memory               = var.ram
+  guest_id             = var.vm-guest-id
   network_interface {
-    network_id = data.vsphere_network.network.id
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
   disk {
-    label = var.vm_name
-    size  = 16
+    label            = "${var.name}-disk"
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    size             = var.disksize == "" ? data.vsphere_virtual_machine.template.disks.0.size : var.disksize 
   }
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
-    customize {
-      linux_options {
-        host_name = "var.vm_name"
-        domain    = " "
-      }
-      network_interface {
-        ipv4_address = "10.200.110.100"
-        ipv4_netmask = 24
-      }
-      ipv4_gateway = "10.200.110.1"
-    }
+  }
+  extra_config = {
+    "guestinfo.metadata"          = base64encode(templatefile("${path.module}/templates/metadata.yaml", local.templatevars))
+    "guestinfo.metadata.encoding" = "base64"
+    "guestinfo.userdata"          = base64encode(templatefile("${path.module}/templates/userdata.yaml", local.templatevars))
+    "guestinfo.userdata.encoding" = "base64"
+  }
+  lifecycle {
+    ignore_changes = [
+      annotation,
+      clone[0].template_uuid,
+      clone[0].customize[0].dns_server_list,
+      clone[0].customize[0].network_interface[0]
+    ]
   }
 }
