0e5302c2a6cf8f804579029661ff539eb7b290f1
hatedone01 <rasmusnk@gmail.com>
2023-04-07 17:08:13 +0200

    change variable.tfvars to variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "vsphere_user" {
+ type = string
+ default = ""
+ description = "vsphere user"
+}
+
+variable "vsphere_password" {
+ type = string
+ default = ""
+ description = "vsphere_password"
+}
+
+variable "vsphere_server" {
+ type = string
+ default = ""
+ description = "vsphere server"
+}
